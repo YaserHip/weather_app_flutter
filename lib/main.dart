1c41689509706f159fdc44406fe629bf5b12c2ea
Yaseer <yaser.hip@gmail.com>
2023-09-29 21:02:05 -0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: App()));
+}
+
+class App extends ConsumerWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+
+}
